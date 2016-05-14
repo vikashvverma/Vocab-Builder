0bfe005b5f725fcb7f66f56ac547ee67de2b5c64
Vikash <vikashkv@thoughtworks.com>
2016-05-15 01:50:53 +0530

    Show saved words

--- app/js/app/saved/saved-controller.js
+++ app/js/app/saved/saved-controller.js
@@ -2,21 +2,26 @@ angular.module('vocabBuilder.controllers')
 
     .controller('SavedController', function ($scope, $log, $timeout, $ionicHistory, HelperService, DictionaryService, store, ionicMaterialInk) {
         $scope.words = store.get("words") || [];
-        $scope.saved = store.get("search-results") || [];
+        $scope.saved = store.get("results") || [];
         $scope.shouldShowDelete = false;
         $scope.shouldShowReorder = false;
         $scope.listCanSwipe = true;
         $scope.remove = function (id) {
+            var index = -1;
             for (var i = 0; i < $scope.words.length; i++) {
                 if ($scope.words[i].id == id) {
+                    index = i;
                     break;
                 }
             }
-            if (i < $scope.words.length) {
+            if (index >= 0) {
                 $scope.words.splice(i, 1);
             }
             store.set("words", $scope.words);
             $scope.shouldShowDelete = false;
+        };
+        $scope.goto = function (word) {
+            DictionaryService.store(word);
         };
         $timeout(function () {
             ionicMaterialInk.displayEffect();
@@ -28,7 +33,7 @@ angular.module('vocabBuilder.controllers')
         $scope.wotd = words.filter(function (word) {
             return word.id == id;
         })[0];
-        $scope.audio = $scope.wotd.audio ? new Audio($scope.wotd.audio) : undefined;
+        $scope.audio = $scope.wotd && $scope.wotd.audio ? new Audio($scope.wotd.audio) : undefined;
         $scope.play = function () {
             $scope.audio ? $scope.audio.play() : undefined;
         };
--- app/js/app/search/search-result-controller.js
+++ app/js/app/search/search-result-controller.js
@@ -1,16 +1,42 @@
 angular.module('vocabBuilder.controllers')
 
-    .controller('SearchResultController', function ($scope, $log, $timeout, $ionicHistory, HelperService, DictionaryService) {
+    .controller('SearchResultController', function ($scope, $log, $timeout, $ionicHistory, HelperService, DictionaryService, store) {
         $scope.result = DictionaryService.retrieve();
         if (!$scope.result && !$scope.result.word) {
             HelperService.notify("Could not find any result!", "info");
             $ionicHistory.goBack();
         }
         if ($scope.result.audio) {
-            $scope.result.audio = new Audio($scope.result.audio);
+            $scope.audio = new Audio($scope.result.audio);
         }
+        $scope.isSaved = (store.get("results")?store.get("results"):[]).map(function (word) {
+            return word.word == $scope.result.word;
+        }).length;
+        $scope.toggle = function (doSave) {
+            var results = store.get("results") || [];
+            var index = -1;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].word == $scope.result.word) {
+                    index = i;
+                    break;
+                }
+            }
+            if (index >= 0) {
+                results.splice(i, 1);
+            }
+            if (doSave) {
+                results.unshift($scope.result);
+                $scope.isSaved = true;
+                HelperService.notify("Saved!", "success");
+            } else {
+                $scope.isSaved = false;
+                HelperService.notify("Removed!", "info");
+
+            }
+            save(results);
+        };
         $scope.play = function () {
-            $scope.result.audio.play()
+            $scope.audio.play()
         };
         $scope.expandCallback = function (index, id) {
             $('#related-words #type' + index).show(1000);
@@ -19,6 +45,9 @@ angular.module('vocabBuilder.controllers')
         $scope.collapseCallback = function (index, id) {
             $('#related-words #type' + index).hide(1000);
         };
+        function save(results) {
+            store.set("results", results);
+        }
     });
 
 
